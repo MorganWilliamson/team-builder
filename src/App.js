fb920a2bf2e5cb22ea56404ab907ca320dfa1a42
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-13 15:35:25 -0500

    Setting up state in App.js.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [teamMembers, setTeamMembers] = useState([])
+
+  const initialFormValues = {
+    name: "", 
+    email: "",
+    role: "",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>i'm a website</p>
     </div>
   );
 }
 
-export default App;
