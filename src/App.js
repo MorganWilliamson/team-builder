ae0ab2b7e746d8a1e424bc218987b8f5e0a14df3
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-13 16:37:37 -0500

    App function built. Form renders data, can add a new team member.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/Form";
+import axios from "../src/axios/index";
+import TeamMember from "./components/TeamMember";
+
+
+const initialFormValues = {
+  name: "", 
+  email: "",
+  role: "",
+};
 
 export default function App() {
   const [teamMembers, setTeamMembers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
 
-  const initialFormValues = {
-    name: "", 
-    email: "",
-    role: "",
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue,
+    });
   };
 
+  const submitForm = () => {
+    const newTeamMember ={
+      name: formValues.name.trim(), 
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    
+    axios
+    .post("fakeapi.com", newTeamMember)
+    .then((res) => {
+      setTeamMembers([...teamMembers, res.data]);
+      setFormValues(initialFormValues)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    axios.get("fakeapi.com")
+      .then(res => setTeamMembers(res.data))
+  }, []);
+    
   return (
-    <div className="App">
-      <p>i'm a website</p>
+    <div className="app-container">
+      <h1>i'm a website</h1>
+
+      <Form
+      values={formValues}
+      update={updateForm}
+      submit={submitForm}
+      />
+
+      {
+        teamMembers.map(teamMember => {
+          return (
+            <TeamMember key={teamMember.id} details={teamMember} />
+          )
+        }) 
+      }
+
     </div>
   );
 }
