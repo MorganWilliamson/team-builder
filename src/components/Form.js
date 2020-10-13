e044b52120c6f6ebf7a59d77c2689709df45792a
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-13 18:50:53 -0500

    Last commit of the night. MVP complete, some stretch attempted.

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,7 @@ export default function Form(props) {
     const {values, update, submit} = props;
 
     const onChange = event => {
-        
         const {name, value} = event.target;
-
         update (name, value);
     };
 
